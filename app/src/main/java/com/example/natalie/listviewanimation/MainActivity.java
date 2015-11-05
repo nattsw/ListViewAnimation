fc2b226f62cb5282f0351094854737125dc51279
Natalie Tay <natalie.tay@gmail.com>
2015-11-05 17:47:38 +0800

    OMG EVERYTHING WURKZ

--- app/src/main/java/com/example/natalie/listviewanimation/MainActivity.java
+++ app/src/main/java/com/example/natalie/listviewanimation/MainActivity.java
@@ -2,29 +2,52 @@ package com.example.natalie.listviewanimation;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.view.View;
-import android.widget.BaseAdapter;
-import android.widget.ListView;
+import android.view.ViewGroup;
+import android.widget.Toast;
 
+import com.nhaarman.listviewanimations.ArrayAdapter;
 import com.nhaarman.listviewanimations.appearance.AnimationAdapter;
 import com.nhaarman.listviewanimations.appearance.simple.SwingRightInAnimationAdapter;
+import com.nhaarman.listviewanimations.itemmanipulation.DynamicListView;
+import com.nhaarman.listviewanimations.itemmanipulation.swipedismiss.OnDismissCallback;
+import com.nhaarman.listviewanimations.itemmanipulation.swipedismiss.SwipeDismissAdapter;
+import com.nhaarman.listviewanimations.itemmanipulation.swipedismiss.undo.SimpleSwipeUndoAdapter;
 
 import java.util.ArrayList;
 
 public class MainActivity extends Activity {
-    private BaseAdapter mAdapter;
+    private ArrayAdapter<String> mAdapter;
     private AnimationAdapter mAnimAdapter;
-    private ListView mNotificationList;
+    private DynamicListView mNotificationList;
+    private ArrayList<String> data;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        initializeList();
-
+        initializeData();
+        initializeAdapter();
         initializeFab();
+    }
+
+    private void initializeData() {
+        data = new ArrayList<>();
+        data.add("A");
+        data.add("B");
+        data.add("C");
+        data.add("D");
+    }
+
+    private void initializeAdapter() {
+        /*Choose which animation you want*/
+
+//        setSwingFromRightAdapter();
+        setUndoSwipeAdapter();
+//        setSwipeDismissAdapter();
     }
 
     private void initializeFab() {
@@ -32,23 +55,56 @@ public class MainActivity extends Activity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                setRightAdapter();
+                initializeAdapter();
             }
         });
     }
 
-    private void initializeList() {
-        mNotificationList = (ListView) findViewById(R.id.notification_list);
-        ArrayList<String> a = new ArrayList<>();
-        a.add("A");
-        a.add("B");
-        mAdapter = new NotificationAreaListViewAdapter(this, a);
-        setRightAdapter();
+    private void initializeList(boolean undo) {
+        mNotificationList = (DynamicListView) findViewById(R.id.notification_list);
+        if (undo) {
+            mAdapter = new MessageCentreListViewAdapter(this, data);
+        } else {
+            mAdapter = new NotificationAreaListViewAdapter(this, data);
+        }
     }
 
-    private void setRightAdapter() {
+    private void setUndoSwipeAdapter() {
+        initializeList(true);
+
+        SimpleSwipeUndoAdapter simpleSwipeUndoAdapter =
+                new SimpleSwipeUndoAdapter(mAdapter, this, new MyOnDismissCallback());
+        mAnimAdapter = new SwingRightInAnimationAdapter(simpleSwipeUndoAdapter);
+        mAnimAdapter.setAbsListView(mNotificationList);
+        mNotificationList.setAdapter(mAnimAdapter);
+        mNotificationList.enableSimpleSwipeUndo();
+    }
+
+    private void setSwipeDismissAdapter() {
+        initializeList(false);
+        SwipeDismissAdapter swipeDismissAdapter =
+                new SwipeDismissAdapter(mAdapter, new MyOnDismissCallback());
+        mAnimAdapter = new SwingRightInAnimationAdapter(swipeDismissAdapter);
+        mAnimAdapter.setAbsListView(mNotificationList);
+        mNotificationList.setAdapter(mAnimAdapter);
+        mNotificationList.enableSwipeToDismiss(new MyOnDismissCallback());
+    }
+
+    private void setSwingFromRightAdapter() {
+        initializeList(false);
         mAnimAdapter = new SwingRightInAnimationAdapter(mAdapter);
         mAnimAdapter.setAbsListView(mNotificationList);
         mNotificationList.setAdapter(mAnimAdapter);
+    }
+
+    private class MyOnDismissCallback implements OnDismissCallback {
+        @Override
+        public void onDismiss(@NonNull final ViewGroup listView,
+                              @NonNull final int[] reverseSortedPositions) {
+            for (int position : reverseSortedPositions) {
+                mAdapter.remove(position);
+            }
+            Toast.makeText(MainActivity.this,"Removed",Toast.LENGTH_LONG).show();
+        }
     }
 }
--- app/src/main/java/com/example/natalie/listviewanimation/MessageCentreListViewAdapter.java
+++ app/src/main/java/com/example/natalie/listviewanimation/MessageCentreListViewAdapter.java
@@ -8,17 +8,16 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import com.nhaarman.listviewanimations.ArrayAdapter;
-import com.nhaarman.listviewanimations.itemmanipulation.swipedismiss.SwipeDismissAdapter;
 import com.nhaarman.listviewanimations.itemmanipulation.swipedismiss.undo.UndoAdapter;
 
 import java.util.ArrayList;
 
-public class NotificationAreaListViewAdapter extends ArrayAdapter<String> {
+public class MessageCentreListViewAdapter extends ArrayAdapter<String> implements UndoAdapter {
     Context mContext;
     ArrayList<String> notifications;
     private static LayoutInflater mInflater = null;
 
-    public NotificationAreaListViewAdapter(Context context, ArrayList<String> notifications) {
+    public MessageCentreListViewAdapter(Context context, ArrayList<String> notifications) {
         this.mContext = context;
         this.notifications = notifications;
 
@@ -42,5 +41,21 @@ public class NotificationAreaListViewAdapter extends ArrayAdapter<String> {
         textView.setText(getItem(position));
 
         return rowView;
+    }
+
+    @NonNull
+    @Override
+    public View getUndoView(int position, View convertView, ViewGroup parent) {
+        View view = convertView;
+        if (view == null) {
+            view = LayoutInflater.from(mContext).inflate(R.layout.undo_row, parent, false);
+        }
+        return view;
+    }
+
+    @NonNull
+    @Override
+    public View getUndoClickView(@NonNull View view) {
+        return view.findViewById(R.id.undo_row_undobutton);
     }
 }
