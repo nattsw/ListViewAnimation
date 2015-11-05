9682e3312e9c46edf30dbb9ef51b32770d4aa3ac
Natalie Tay <natalie.tay@gmail.com>
2015-11-05 16:13:12 +0800

    Match layout to mockup.

--- app/src/main/java/com/example/natalie/listviewanimation/MainActivity.java
+++ app/src/main/java/com/example/natalie/listviewanimation/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.natalie.listviewanimation;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.view.View;
 import android.widget.BaseAdapter;
 import android.widget.ListView;
 
@@ -19,6 +22,16 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+                setRightAdapter();
+            }
+        });
 
         initializeList();
     }
